2a7e2c1deb7f7ff7342b97b9d07ee57a16636aa6
Joshua Tauberer <jt@occams.info>
2017-07-10 09:45:38 -0400

    slight refactor in start.bat

--- start.bat
+++ start.bat
@@ -1,14 +1,14 @@
 @echo off
 
 REM EDIT THIS LINE DEPENDING ON WHERE WinPython IS INSTALLED.
-set WP=.\WinPython-32bit-3.4.4.1Zero
+set WP=.\WinPython-32bit-3.4.4.1Zero\python-3.4.4
 REM THERE IS NOTHING ELSE TO CHANGE IN THIS FILE.
 
 REM Must load Python environment before running pip.
-call %WP%\scripts\env.bat
+call %WP%\..\scripts\env.bat
 
 REM Check that Python modules are installed...
-%WP%\python-3.4.4\Scripts\pip.exe install -r requirements.txt
+%WP%\Scripts\pip.exe install -r requirements.txt
 
 REM Run the CRS scraper...
-%WP%\python-3.4.4\python.exe crs_scraper.py
+%WP%\python.exe crs_scraper.py
